fed214f66ad8f02e9cafb84edc51b6cbdfa5caaf
Vitalik Kim <kimv1086@gmail.com>
2020-02-02 23:12:44 +0600

    loading file

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require('express');
-const messages = require('./messages');
+const messages = require('./app/messages');
 const app = express();
-
-const port = 8000;
+const port = 9000;
 
 app.use(express.json());
 
-app.use('./messages', messages);
+app.use('/messages', messages);
 
 app.listen(port, () => {
     console.log(`Server started on ${port} port`);
